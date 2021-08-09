700e9f0c6d99d05eaf9bde8dc89b2f98e4bd8fe7
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-08-09 11:53:47 +0200

    commit

--- src/test/java/pl/kmiecik/designpatternslambda/ChainConsumes.java
+++ src/test/java/pl/kmiecik/designpatternslambda/ChainConsumes.java
@@ -7,11 +7,27 @@ import java.util.function.Consumer;
 public class ChainConsumes {
 
     @Test
+    public void shouldChainConsumesTooLong(){
+        Consumer<String> c1 =(String s) -> System.out.println("c1="+s);
+        Consumer<String> c2 =(String s) -> System.out.println("c2="+s);
+        Consumer<String> c3 = (String s) ->{
+            c1.accept("aaa");
+            c2.accept("bbb"+ s);
+        };
+        c3.accept("test");
+    }
+
+    @Test
     public void shouldChainConsumes(){
         Consumer<String> c1 =(String s) -> System.out.println("c1="+s);
         Consumer<String> c2 =(String s) -> System.out.println("c2="+s);
-        Consumer<String> c3 = c1.andThen(c2);
+        Consumer<String> c3 = (String s) ->{
+            c1.accept("aaa");
+            c2.accept("bbb"+ s);
+        };
         c3.accept("test");
     }
+
+
 
 }
